0d1e4781f2cb4f8395e80d8e4de01c61d6940a0d
Arthur Clay Odom <thekingofthebritons@gmail.com>
2020-10-21 11:34:37 -0400

    No longer exposing keys to the world

--- config.js
+++
@@ -1,7 +0,0 @@
-// Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
-}
